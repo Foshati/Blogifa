074d9de7c6ec24c275748dc7e4ecf4295af0cbe5
Foshati <amirrezafoshati@gmail.com>
2024-05-17 18:12:59 +0330

    fixed header components all page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,60 @@
+import Image from "next/image";
 
 function App() {
   return (
-    <div >
+    // mt-[70px]
+    <div className="flex ">
+      <div className="flex flex-col">
+        <h2>About Agency</h2>
+        <h3>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h3>
+        <p>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.s
+        </p>
 
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-4 text-center">
+              <div class="p-4 sm:w-1/4 w-1/2">
+                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                  2.7K
+                </h2>
+                <p class="leading-relaxed">Users</p>
+              </div>
+              <div class="p-4 sm:w-1/4 w-1/2">
+                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                  1.8K
+                </h2>
+                <p class="leading-relaxed">Subscribes</p>
+              </div>
+              <div class="p-4 sm:w-1/4 w-1/2">
+                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                  35
+                </h2>
+                <p class="leading-relaxed">Downloads</p>
+              </div>
+              <div class="p-4 sm:w-1/4 w-1/2">
+                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+                  4
+                </h2>
+                <p class="leading-relaxed">Products</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div>
+        <Image
+          src="/image/aboutfa.gif"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,45 +1,102 @@
 "use client";
-import { BookOpen, PlayIcon } from "lucide-react";
-import { Search } from "lucide-react";
-import { Fingerprint } from "lucide-react";
-
+import { useEffect, useState } from "react";
+import { BookOpen, PlayIcon, Search, Fingerprint } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import MusicIco from "@/components/PlayMusic/playMusic";
 
 export const Navbar = () => {
-  const PathName = usePathname();
+  const pathname = usePathname();
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 20) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
-      <header className="fixed top-0 left-0 z-50 w-full bg-black bg-opacity-50 navbar">
-        <div className="mx-auto navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+      {/* pathname === "/" || =>>  With this condition, the navbar will be placed on the main page banner   */}
+      <header
+        className={`w-full mx-auto bg-black z-40 bg-opacity-50 rounded-b-2xl transition-all  duration-300 ease-in-out transform 
+        ${pathname === "/" || isSticky ? "fixed top-0  left-0 z-50" : ""}`}
+      >
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li className="hover:text-red-500">
+                  <Link
+                    href="/"
+                    className={pathname === "/" ? "text-red-500" : ""}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="hover:text-red-500">
+                  <Link
+                    href="/about"
+                    className={pathname === "/about" ? "text-red-500" : ""}
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className="hover:text-red-500">
+                  <Link
+                    href="/contact"
+                    className={pathname === "/contact" ? "text-red-500" : ""}
+                  >
+                    Contact
+                  </Link>
+                </li>
+                <li className="hover:text-red-500">
+                  <Link
+                    href="/blog"
+                    className={pathname === "/blog" ? "text-red-500" : ""}
+                  >
+                    Blog
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content  mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
+            <BookOpen className="m-3 text-yellow-500 size-7" />
+          </div>
+          <div className="hidden navbar-center lg:flex">
+            <ul className="px-1 menu menu-horizontal">
               <li className="hover:text-red-500">
                 <Link
                   href="/"
-                  className={PathName == "/" ? "text-red-500" : ""}
+                  className={pathname === "/" ? "text-red-500" : ""}
                 >
                   Home
                 </Link>
@@ -47,7 +104,7 @@ export const Navbar = () => {
               <li className="hover:text-red-500">
                 <Link
                   href="/about"
-                  className={PathName == "/about" ? "text-red-500" : ""}
+                  className={pathname === "/about" ? "text-red-500" : ""}
                 >
                   About
                 </Link>
@@ -55,7 +112,7 @@ export const Navbar = () => {
               <li className="hover:text-red-500">
                 <Link
                   href="/contact"
-                  className={PathName == "/contact" ? "text-red-500" : ""}
+                  className={pathname === "/contact" ? "text-red-500" : ""}
                 >
                   Contact
                 </Link>
@@ -63,58 +120,24 @@ export const Navbar = () => {
               <li className="hover:text-red-500">
                 <Link
                   href="/blog"
-                  className={PathName == "/blog" ? "text-red-500" : ""}
+                  className={pathname === "/blog" ? "text-red-500" : ""}
                 >
                   Blog
                 </Link>
               </li>
             </ul>
           </div>
-          <BookOpen className="m-3 text-yellow-500 size-7" />
-        </div>
-        <div className="hidden navbar-center lg:flex">
-          <ul className="px-1 menu menu-horizontal">
-            <li className="hover:text-red-500">
-              <Link href="/" className={PathName == "/" ? "text-red-500" : ""}>
-                Home
-              </Link>
-            </li>
-            <li className="hover:text-red-500">
-              <Link
-                href="/about"
-                className={PathName == "/about" ? "text-red-500" : ""}
-              >
-                About
-              </Link>
-            </li>
-            <li className="hover:text-red-500">
-              <Link
-                href="/contact"
-                className={PathName == "/contact" ? "text-red-500" : ""}
-              >
-                Contact
-              </Link>
-            </li>
-            <li className="hover:text-red-500">
-              <Link
-                href="/blog"
-                className={PathName == "/blog" ? "text-red-500" : ""}
-              >
-                Blog
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <div className="btn btn-ghost btn-circle">
-            <MusicIco />
+          <div className="navbar-end">
+            <div className="btn btn-ghost btn-circle">
+              <MusicIco />
+            </div>
+            <button className="hidden btn btn-ghost btn-circle lg:flex">
+              <Search />
+            </button>
+            <button className="btn btn-ghost btn-circle">
+              <Fingerprint />
+            </button>
           </div>
-          <button className="hidden btn btn-ghost btn-circle lg:flex">
-            <Search />
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <Fingerprint />
-          </button>
         </div>
       </header>
     </>
